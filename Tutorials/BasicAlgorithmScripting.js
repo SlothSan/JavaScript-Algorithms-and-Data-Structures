b931d8953279e865714342b31ce1f70609b091de
SlothSan <SlothSan@users.noreply.github.com>
2022-05-24 15:03:47 +0100

    Update BasicAlgorithmScripting.js

--- Tutorials/BasicAlgorithmScripting.js
+++ Tutorials/BasicAlgorithmScripting.js
@@ -92,7 +92,7 @@ function largestOfFour(arr) {
       for (let j = 1; j < arr[i].length; j++) {
         if (arr[i][j] > largestNumber) {
           largestNumber = arr[i][j];
-        }
+        }s
       }
       results[i] = largestNumber;
     }
@@ -103,3 +103,187 @@ function largestOfFour(arr) {
   
   
   largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+
+/*Confirm the Ending
+
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.*/
+
+function confirmEnding(str, target) {
+    return str.slice(str.length - target.length) === target;
+  }
+  
+  confirmEnding("Bastian", "n");
+
+/*Repeat a String Repeat a String
+
+Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.*/
+
+function repeatStringNumTimes(str, num) {
+    let strStr = "";
+    if (num <= 0) {
+      return "";
+    }
+    for (let i = 0; i < num; i++) {
+      strStr += str;
+    }
+    return strStr;
+  }
+  
+  repeatStringNumTimes("abc", 3);
+
+/*Truncate a String
+
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.*/
+
+function truncateString(str, num) {
+    if (str.length > num) {
+      return str.slice(0,num) + "...";
+        }else {
+   return str;
+    }
+  }
+   
+  
+  truncateString("A-tisket a-tasket A green and yellow basket", 8);
+
+/*Finders Keepers
+
+Create a function that looks through an array arr and returns the first element in it that passes a 'truth test'. This means that given an element x, the 'truth test' is passed if func(x) is true. If no element passes the test, return undefined.*/
+
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+      num = arr[i];
+      if (func(num)) {
+        return num;
+      }
+    }
+    return undefined;
+  }
+  
+  findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+/*Boo who
+
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.*/
+
+function booWho(bool) {
+    return typeof bool === "boolean";
+  }
+  
+  booWho(null);
+
+/*Title Case a Sentence
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like the and of.*/
+
+function titleCase(str) {
+    const newTitle = str.split(" ");
+    const updatedTitle = [];
+    for (let st in newTitle) {
+      updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+    }
+  
+    console.log(updatedTitle.join(" "));
+    return updatedTitle.join(" ");
+  }
+  
+  titleCase("I'm a little tea pot");
+
+
+/*ice and Splice
+
+You are given two arrays and an index.
+
+Copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.*/
+
+function frankenSplice(arr1, arr2, n) {
+    let sliceArr2 = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+      sliceArr2.splice(n, 0, arr1[i]);
+      n++
+    }
+    console.log(sliceArr2);
+    return sliceArr2;
+  }
+  
+  frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+/*Falsy Bouncer
+
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.*/
+
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    console.log(newArray);
+    return newArray;
+  }
+  
+  function bouncer(arr) {
+    return arr.filter(Boolean);
+  }
+  
+  bouncer([7, "ate", "", false, 9]);
+
+
+/*Where do I Belong
+
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).*/
+
+function getIndexToIns(arr, num) {
+    arr.sort((a, b) => a - b);
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= num)
+      return i;
+    }
+    console.log(arr);
+    return arr.length;
+  }
+  
+  getIndexToIns([40, 60], 50);
+
+/*Mutations
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string hello does not contain a y.
+
+Lastly, ["Alien", "line"], should return true because all of the letters in line are present in Alien.*/
+
+function mutation(arr) {
+    //push arr to lowercase to make it easier to compare.
+    let test = arr[1].toLowerCase();
+    let target = arr[0].toLowerCase();
+    //Loop through test characters and if any of them is not found return "not found"
+    for (let i = 0; i < test.length; i++){
+      if (target.indexOf(test[i]) < 0) return false;
+    }
+    return true;
+  }
+  
+  mutation(["hello", "hey"]);
