5c768a58b7cb55777f0954eb6a1cba6a27862c2a
SlothSan <SlothSan@users.noreply.github.com>
2022-05-24 15:59:55 +0100

    Update BasicAlgorithmScripting.js

--- Tutorials/BasicAlgorithmScripting.js
+++ Tutorials/BasicAlgorithmScripting.js
@@ -287,3 +287,21 @@ function mutation(arr) {
   }
   
   mutation(["hello", "hey"]);
+
+/*Chunky Monkey
+
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.*/
+
+function chunkArrayInGroups(arr, size) {
+    var newArray = [];
+    let i = 0;
+  
+    while (i < arr.length) {
+      newArray.push(arr.slice(i, i + size));
+      i += size;
+    }
+    console.log(newArray);
+    return newArray;
+  }
+  
+  chunkArrayInGroups(["a", "b", "c", "d"], 2);
