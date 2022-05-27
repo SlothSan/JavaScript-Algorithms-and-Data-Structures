23367de19770d67600f490b3f23101c1cc9b7aae
SlothSan <SlothSan@users.noreply.github.com>
2022-05-27 15:51:16 +0100

    More tutorials.

--- Pluralsight/js-demo/02/demos/after/js-demo/home.js
+++ Pluralsight/js-demo/02/demos/after/js-demo/home.js
@@ -9,9 +9,31 @@ let message = (price < 10) ? 'Yes' : "No";
 
 showMessage(message);
 
-let myFunction = function logging () {
-    console.log("Here is a message");
+let myFunction = function (message, firstName) {
+    console.log(message);
+    console.log(firstName);
 }
 
-logMessage();
-logMessage();
+myFunction("Hello", "Mike");
+
+
+changePercentOff(30);
+
+
+let key = 42;
+function getSecretCode(value) {
+    
+    let keyGenerator = function() {
+        let key = 12;
+        console.log('in keyGenerator', key);
+        return key;
+    }
+    
+    let code = value * keyGenerator();
+    cosnole.log("in secretCode", key);
+    return code;
+}
+
+let secretCode = getSecretCode(2);
+
+
