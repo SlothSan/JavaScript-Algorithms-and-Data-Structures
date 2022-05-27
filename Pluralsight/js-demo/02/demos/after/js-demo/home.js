a5c19473220aa3ecbc5695a3ebb76bb91bf0b16d
SlothSan <SlothSan@users.noreply.github.com>
2022-05-27 15:26:41 +0100

    Notes from Pluralsight.

--- Pluralsight/js-demo/02/demos/after/js-demo/home.js
+++ Pluralsight/js-demo/02/demos/after/js-demo/home.js
@@ -1,1 +1,17 @@
-showMessage("More Changes !");
+let price = 20,
+    name = 'Hiking Boots',
+    discounted = false,
+    taxRate = 0.07;
+
+
+
+let message = (price < 10) ? 'Yes' : "No";
+
+showMessage(message);
+
+let myFunction = function logging () {
+    console.log("Here is a message");
+}
+
+logMessage();
+logMessage();
