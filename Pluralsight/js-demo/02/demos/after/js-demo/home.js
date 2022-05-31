d41246bd365d07d543cddcc22705812fe8be69a7
SlothSan <SlothSan@users.noreply.github.com>
2022-05-31 10:33:46 +0100

    JS Pluralsight tutorials.

--- Pluralsight/js-demo/02/demos/after/js-demo/home.js
+++ Pluralsight/js-demo/02/demos/after/js-demo/home.js
@@ -1,4 +1,4 @@
-let price = 20,
+/*let price = 20,
     name = 'Hiking Boots',
     discounted = false,
     taxRate = 0.07;
@@ -7,7 +7,64 @@ let price = 20,
 
 let message = (price < 10) ? 'Yes' : "No";
 
-showMessage(message);
+showMessage(message);*/
+
+/*let mySymbol = Symbol();
+
+let person = {
+    name: 'John',
+    age: 32,
+    partTime: false,
+    [mySymbol]: 'secretInformation',
+    showInfo: function(realAge) {
+        showMessage(this.name + " is " + realAge)
+    }
+};
+
+person.age = 40;
+person['name'] = 'Mike';
+
+showMessage( typeof person.showInfo);
+*/
+
+
+/*let person = {
+    name: 'John',
+    age: 32,
+    partTime: false
+}
+
+function incrementAge(person){
+    person.age++;
+}
+
+incrementAge(person);
+
+showMessage(person.age);
+*/
+
+/*
+let s = "hello";
+
+showMessage(s.charAt(0));
+*/
+
+const button = document.getElementById('see-review');
+
+button.addEventListener('click', function(){
+    const review = document.getElementById('review');
+    if(review.classList.contains('d-none')) {
+        review.classList.remove('d-none');
+        button.textContent = "Close Review";
+    }
+    else {
+        review.classList.add('d-none');
+        button.textContent = "See Review";
+    }
+});
+
+
+
 
 let myFunction = function (message, firstName) {
     console.log(message);
@@ -35,5 +92,6 @@ function getSecretCode(value) {
 }
 
 let secretCode = getSecretCode(2);
+
 
 
--- Tutorials/FunctionalProgramming.js
+++ Tutorials/FunctionalProgramming.js
@@ -405,3 +405,21 @@ You might learn a lot about the map method if you implement your own version of 
 
 Write your own Array.prototype.myMap(), which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The Array instance can be accessed in the myMap method using this.*/
 
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  let newArray = [];
+  // Only change code below this line
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callback(this[i]));
+  }
+  // Only change code above this line
+  console.log(newArray);
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
