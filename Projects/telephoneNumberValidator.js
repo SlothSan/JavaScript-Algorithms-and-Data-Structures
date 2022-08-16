fbbb25014417f093946c75682f862b6b24957041
Mike Oram <Mikeoram01@gmail.com>
2022-08-16 08:04:26 +0100

    Completed the JS project for FreeCodeCamp.

---
+++ Projects/telephoneNumberValidator.js
@@ -0,0 +1,19 @@
+function telephoneCheck(str) {
+	let result = false;
+	let formats = [
+		/^\d\d\d\d\d\d\d\d\d\d$/,			// 5555555555
+		/^\d\d\d-\d\d\d-\d\d\d\d/,			// 555-555-5555
+		/^\d\d\d\s\d\d\d\s\d\d\d\d/,		// 555 555 5555
+		/^\(\d\d\d\)\d\d\d-\d\d\d\d/,		// (555)555-5555
+		/^\(\d\d\d\)\s\d\d\d-\d\d\d\d/,		// (555) 555-5555
+		/^1\d\d\d\d\d\d\d\d\d\d/,			// 15555555555
+		/^1\s\d\d\d\s\d\d\d\s\d\d\d\d/,		// 1 555 555 5555
+		/^1\s\d\d\d-\d\d\d-\d\d\d\d/,		// 1 555-555-5555
+		/^1\(\d\d\d\)\d\d\d-\d\d\d\d/,		// 1(555)555-5555
+		/^1\s\(\d\d\d\)\s\d\d\d-\d\d\d\d/	// 1 (555) 555-5555
+	];
+
+	result = formats.some(reg => reg.test(str));
+	
+  return result;
+}
